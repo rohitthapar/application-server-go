61ed7e552d823b2d93338be5108227cc8a4c04e6
Rohit Thapar <thaprt206@gmail.com>
2023-05-30 08:19:44 +0530

    Add M

--- main.go
+++ main.go
@@ -36,6 +36,34 @@ func bookById(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, book)
 }
 
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Not enough books"})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+	 
+
+}
+
+
 func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
@@ -61,5 +89,6 @@ func main() {
 	router.GET("/books", getBooks)
 	router.GET("/books/:id",bookById)
 	router.POST("/books",createBook )
+	router.PATCH("/checkout",checkoutBook)
 	router.Run("localhost:8080")
 }
