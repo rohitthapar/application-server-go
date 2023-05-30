7fd142bef54b5523107d1f0267f86b1dedb60d80
Rohit Thapar <thaprt206@gmail.com>
2023-05-30 08:02:21 +0530

    Add M

--- main.go
+++ main.go
@@ -29,7 +29,8 @@ func bookById(c *gin.Context) {
 	book, err := getBookById(id)
 
 	if err != nil {
-		return 
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
 	}
 
 	c.IndentedJSON(http.StatusOK, book)
